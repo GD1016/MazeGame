045eb31a77f2de3c98f63f190a07e7d62cd01228
Code Fox <development@code-fox.de>
2017-07-03 11:05:47 +0200

    added random move script for AI

---
+++ Assets/RandWalkerAI.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RandWalkerAI : MonoBehaviour {
+
+    public Character Character;
+
+    // Use this for initialization
+    void Start() {
+
+    }
+
+    // Update is called once per frame
+    void Update() {
+        int random = Random.Range(0, 4);
+
+        if (random == 0) {
+            Character.move(Character.Direction.left);
+            return;
+        }
+        if (random == 1) {
+            Character.move(Character.Direction.right);
+            return;
+        }
+        if (random == 2) {
+            Character.move(Character.Direction.up);
+            return;
+        }
+        if (random == 3) {
+            Character.move(Character.Direction.down);
+            return;
+        }
+    }
+}
