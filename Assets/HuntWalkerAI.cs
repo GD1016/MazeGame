0202db0a31f934d8b4af344742c43f167173c500
Code Fox <development@code-fox.de>
2017-07-10 14:26:27 +0200

    created an implemented Queue

---
+++ Assets/HuntWalkerAI.cs
@@ -0,0 +1,74 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class HuntWalkerAI : MonoBehaviour {
+
+    public Character character;
+    float timer = 0.5f; // timer variable, used as a "countdown" to next move
+    int[,] distance;
+
+    // Use this for initialization
+    void Start() {
+
+    }
+
+    // Update is called once per frame
+    void Update() {
+        timer -= Time.deltaTime; // subtract time since last frame
+        if (timer > 0f) { // if countdown not done, skip
+            return;
+        }
+        timer = 0.5f; // if we get here, reset timer
+        Move(); // and move
+    }
+
+    private void Move() {
+
+    }
+
+    void Breitensuche(IndexPaar source) {
+        Queue<IndexPaar> queue = new Queue<IndexPaar>();
+        queue.Enqueue(source);
+        distance = new int[character.board.size, character.board.size];
+
+        for (int i = 0; i < distance.GetLength(0); i++) {
+            for (int j = 0; j < distance.GetLength(1); j++) {
+                distance[i, j] = int.MaxValue;
+            }
+        }
+        distance[source.col, source.row] = 0;
+
+        while(queue.Count > 0) {
+            IndexPaar u = queue.Dequeue();
+            List<IndexPaar> neighbours = character.board.GetWalkableNeighbours(u);
+            foreach (IndexPaar neighbour in neighbours) {
+                if (distance[neighbour.col, neighbour.row] == int.MaxValue) {
+                    distance[neighbour.col, neighbour.row] = distance[u.col, u.row] + 1;
+                    queue.Enqueue(neighbour);
+                }
+            }
+        }
+    }
+    List<IndexPaar> GetPath(IndexPaar target) {
+        Breitensuche(character.pos);
+        List<IndexPaar> path = new List<IndexPaar>();
+
+        if(distance[target.col, target.row] < int.MaxValue) {
+            IndexPaar node = target;
+            while (distance[node.col, node.row] != 0) {
+                path.Add(node);
+                List<IndexPaar> neighbours = character.board.GetWalkableNeighbours(node);
+                foreach(IndexPaar neighbour in neighbours) {
+                    if (distance[neighbour.col, neighbour.row] == distance[node.col, node.row] - 1) {
+                        node = neighbour;
+                        break;
+                    }
+                }
+            }
+        }
+        path.Reverse();
+        return path;
+    }
+}
