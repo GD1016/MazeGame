b88cc0b1abd596d90ec0ff84844984d562de4060
Code Fox <development@code-fox.de>
2017-07-10 12:19:40 +0200

    Added MarkMoveArea()

--- Assets/MazeGame.cs
+++ Assets/MazeGame.cs
@@ -26,5 +26,37 @@ public class MazeGame : MonoBehaviour {
         if (Input.GetKeyDown(KeyCode.D)) {
             player.Move(Direction.right);
         }
+        if (Input.GetKeyDown(KeyCode.Mouse0)) {
+            Vector3 mouseWorldPos = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+            int indexX = (int)mouseWorldPos.x;
+            int indexY = (int)mouseWorldPos.y;
+            IndexPaar source = new IndexPaar();
+            source.col = indexX;
+            source.row = indexY;
+            MarkMoveArea(source, 3);
+        }
+    }
+    void MarkMoveArea(IndexPaar source, int range) {
+        if (range < 0)
+            return;
+
+        if (board.IsAlive(source.col, source.row)) {
+
+            board.grid[source.col, source.row].GetComponent<Renderer>().material.color = Color.red;
+
+            IndexPaar left = source;
+            left.col--;
+            IndexPaar right = source;
+            right.col++;
+            IndexPaar up = source;
+            up.row++;
+            IndexPaar down = source;
+            down.row--;
+
+            MarkMoveArea(left, range - 1);
+            MarkMoveArea(right, range - 1);
+            MarkMoveArea(up, range - 1);
+            MarkMoveArea(down, range - 1);
+        }
     }
 }
